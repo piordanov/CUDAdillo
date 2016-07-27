39140d86d11350d0f136d213a1972b299d47c3dc
Peter Iordanov <iordano2@illinois.edu>
2016-07-27 16:57:50 -0500

    added cuda tests

--- main.cpp
+++ main.cpp
@@ -1,28 +1,75 @@
 #include <QCoreApplication>
 #include <armadillo>
-#include <benchmark/benchmark.h>
+#include <benchmark/benchmark_api.h>
+#include <chrono>
 
 using namespace arma;
 
-static void BM_StringCreation(benchmark::State& state) {
-  while (state.KeepRunning())
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+#define BASIC_BENCHMARK_TEST(x) \
+    BENCHMARK(x)->Arg(8)->Arg(512)->Arg(8192)->Unit(benchmark::kMillisecond)
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  while (state.KeepRunning())
-    std::string copy(x);
+extern "C"
+{
+    double * add(float *A, float *B, int numRows, int numCols);
+    double * multiply(float *A, float *B, int numRows, int numCols);
 }
-BENCHMARK(BM_StringCopy);
+
+static void BM_matrixAdditionCPU(benchmark::State& state) {
+    int n = int(state.range_x());
+    fmat matA(n,n);
+    matA.fill(5);
+    fmat matB(n,n);
+    matB.fill(7);
+    while (state.KeepRunning())
+        fmat temp = matA + matB;
+}
+
+BASIC_BENCHMARK_TEST(BM_matrixAdditionCPU);
+
+static void BM_matrixAdditionGPU(benchmark::State& state) {
+    int n = int(state.range_x());
+    fmat matA(n,n);
+    matA.fill(5);
+    fmat matB(n,n);
+    matB.fill(7);
+    while (state.KeepRunning())
+        add(matA.memptr(),matB.memptr(),n,n);
+}
+BASIC_BENCHMARK_TEST(BM_matrixAdditionGPU);
+
+static void BM_matrixMultiplyCPU(benchmark::State& state) {
+    int n = int(state.range_x());
+    fmat matA(n,n);
+    matA.fill(5);
+    fmat matB(n,n);
+    matB.fill(7);
+    while (state.KeepRunning())
+        fmat temp = matA * matB;
+}
+BASIC_BENCHMARK_TEST(BM_matrixMultiplyCPU);
+
+
+static void BM_matrixMultiplyGPU(benchmark::State& state) {
+    int n = int(state.range_x());
+    fmat matA(n,n);
+    matA.fill(5);
+    fmat matB(n,n);
+    matB.fill(7);
+    while (state.KeepRunning())
+        multiply(matA.memptr(),matB.memptr(),n,n);
+}
+BASIC_BENCHMARK_TEST(BM_matrixMultiplyGPU);
+
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    std::ofstream benchmarks("benchmark.json");
+    std::streambuf *coutbuf = std::cout.rdbuf();
+    std::cout.rdbuf(benchmarks.rdbuf());
+
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::RunSpecifiedBenchmarks();
-    return a.exec();
+
+    std::cout.rdbuf(coutbuf);
 }
