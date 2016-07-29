541f0dcf409fcc1d298f151011abfc3563092c6c
Peter Iordanov <iordano2@illinois.edu>
2016-07-29 13:25:01 -0500

    added test_addition

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace arma;
 
 #define BASIC_BENCHMARK_TEST(x) \
-    BENCHMARK(x)->Arg(8)->Arg(512)->Arg(8192)->Unit(benchmark::kMillisecond)
+    BENCHMARK(x)->Arg(8)->Unit(benchmark::kMillisecond)
 
 static void BM_matrixAdditionCPU(benchmark::State& state) {
     int n = int(state.range_x());
@@ -54,6 +54,26 @@ BASIC_BENCHMARK_TEST(BM_matrixMultiplyCPU);
 //}
 //BASIC_BENCHMARK_TEST(BM_matrixMultiplyGPU);
 
+void test_addition()
+{
+    mat matA = randu<mat>(4,4);
+    mat matB = randu<mat>(4,4);
+
+    mat cpuSum = matA + matB;
+    mat * gpuSum = ArmaCudaWrapper::addMat<double>(&matA,&matB);
+    bool same = approx_equal(cpuSum, *gpuSum,"absdiff", 0.01);
+    delete gpuSum;
+    printf("approx-equal: %s\n", same ? "true" : "false");
+    printf("A:\n");
+    matA.print();
+    printf("\nB:\n");
+    matB.print();
+
+    printf("\nCPU Output:\n");
+    cpuSum.print();
+    printf("\nGPU Output:\n");
+    gpuSum->print();
+}
 
 
 int main(int argc, char *argv[])
@@ -61,6 +81,8 @@ int main(int argc, char *argv[])
 //    std::ofstream benchmarks("benchmark.json");
 //    std::streambuf *coutbuf = std::cout.rdbuf();
 //    std::cout.rdbuf(benchmarks.rdbuf());
+
+    test_addition();
 
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::RunSpecifiedBenchmarks();
